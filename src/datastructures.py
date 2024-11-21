7ddb1a2d96294f7be3c154ee5dadfa10c93c2da6
andreinapazz <108377491+andreinapazz@users.noreply.github.com>
2024-11-21 02:35:44 +0000

    13 pruebas aprobadas

--- src/datastructures.py
+++ src/datastructures.py
@@ -1,35 +1,47 @@
 
-"""
-update this file to implement the following already declared methods:
-- add_member: Should add a member to the self._members list
-- delete_member: Should delete a member from the self._members list
-- update_member: Should update a member from the self._members list
-- get_member: Should return a member from the self._members list
-"""
 from random import randint
+
 
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-
-        # example list of members
         self._members = []
+
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
-        return randint(0, 99999999)
+        return randint(0, 9999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        if "id" not in member:
+            member ["id"]=self._generateId()
+
+       
+        self._members.append({
+            "id": member["id"],
+            "first_name": member["first_name"],
+            "age": member ["age"],
+            "lucky_numbers": member["lucky_numbers"]
+            }
+        )
 
     def delete_member(self, id):
+        for member in self._members:
+            if member["id"]==id:
+                self._members.remove(member)
+                return ({"done": True})
+        return ({"done": False})   
+
         # fill this method and update the return
-        pass
+
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member ["id"] == int(id):
+                return member
+        return ({"message":"member do not exist"})    
+        
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
